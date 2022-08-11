734a5e271c9858a75899b4aefead249a469a22eb
Augusto Rogerio <augustoo.rd@gmail.com>
2022-08-11 20:53:54 -0300

    Colocando a categoria de livros

--- app.py
+++ app.py
@@ -4,12 +4,22 @@ app = Flask("projeto")
 @app.route("/")
 def ola_mundo():
     #criar uma variável com o meu nome
-    nome="Augusto Rogerio Domiciano"
+    nome="Augusto Domiciano"
     produtos = [
     {"nome": "PlayStation 5", "preco": 2000.00},
-    {"nome": "Xbox Series X", "preco": 3999.99},
+    {"nome": "Xbox Serie X", "preco": 3999.99},
     {"nome": "Nitendo Switch", "preco": 1000.00}]
 
     return render_template("alo.html", n=nome, aProdutos=produtos), 200
+
+@app.route("/livros")
+def mostra_livros():
+    livros = [
+    {"titulo": "Duna", "autor": "Frank Hebert"},
+    {"titulo": "Cosmos", "autor": "Carl Sagan"},
+    {"titulo": "Os Miseraveis", "autor": "Alexandre Dumas"}]
+
+    return render_template("livros.html", aLivros=livros), 200
+
 
 app.run(debug=True)
