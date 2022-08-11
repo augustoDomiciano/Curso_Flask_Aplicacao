b23542fdbe3dd1e093baea3b37705a817216df8c
Augusto Rogerio <augustoo.rd@gmail.com>
2022-08-11 00:17:17 -0300

    Colocando o template e alterando o app

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
-
+# coding: utf-8
+from flask import Flask, render_template
 app = Flask("projeto")
-
 @app.route("/")
-
 def ola_mundo():
-    return"Olá mundo! Esse é meu primeiro codigo flask"
+    #criar uma variável com o meu nome
+    nome="Augusto Domiciano"
+    produtos = [
+    {"nome": "Ração", "preco": 199.99},
+    {"nome": "Playstation", "preco": 1999.99}]
+
+    return render_template("alo.html", n=nome, aProdutos=produtos), 200
 
 app.run()
