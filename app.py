f5ca66c78e986165eaae1de92d20231e99453c22
Augusto Rogerio <augustoo.rd@gmail.com>
2022-08-12 23:26:17 -0300

    Adicionando as paginas de login e inicial

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 # coding: utf-8
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect,url_for
 app = Flask("projeto")
 @app.route("/")
 def ola_mundo():
     #criar uma variável com o meu nome
-    nome="Augusto Domiciano"
+    nome="Entrar"
     produtos = [
-    {"nome": "PlayStation 5", "preco": 2000.00},
-    {"nome": "Xbox Serie X", "preco": 3999.99},
-    {"nome": "Nitendo Switch", "preco": 1000.00}]
+    {"Categoria": "Livros", "Quantidade": 200},
+    {"Categoria": "Games", "Quantidade": 500},
+    {"Categoria": "Moda", "Quantidade": 100}]
 
     return render_template("alo.html", n=nome, aProdutos=produtos), 200
 
@@ -20,6 +20,27 @@ def mostra_livros():
     {"titulo": "Os Miseraveis", "autor": "Alexandre Dumas"}]
 
     return render_template("livros.html", aLivros=livros), 200
+
+@app.route("/games")
+def mostra_games():
+    games = [
+    {"nome": "PRODUTO", "preco": "PREÇO"},
+    {"nome": "PlayStation 5", "preco": 2000.00},
+    {"nome": "Xbox Serie X", "preco": 3999.99},
+    {"nome": "Nitendo Switch", "preco": 1000.00}]
+
+    return render_template("games.html", aGames=games), 200
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Cadastro incorreto. Por favor, tente novamente mais tarde'
+        else:
+            return redirect(url_for('mostra_livros'))
+    return render_template('login.html', error=error)
 
 
 app.run(debug=True)
